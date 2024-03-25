157e95a0ce74a1c3797d5ee5e11b7b81d3722ffd
MoskaPoska <artem.buravlev333@gmail.com>
2024-03-25 19:07:01 +0200

    Добавьте файлы проекта.

---
+++ ClientServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientServer
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Process.Start("Server.exe");
+            IPAddress address = Dns.GetHostAddresses(Dns.GetHostName())[2];
+            Console.WriteLine("Клиент запущен");
+
+            Console.WriteLine("Введите строку для отправки на сервер:");
+            string message = Console.ReadLine();
+            IPEndPoint endPoint = new IPEndPoint(address, 1024);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            try
+            {
+                socket.Connect(endPoint);
+                if (socket.Connected)
+                {
+                    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                    socket.Send(messageBytes);
+
+                    byte[] responseBytes = new byte[1024];
+                    int len = socket.Receive(responseBytes);
+                    string response = Encoding.UTF8.GetString(responseBytes, 0, len);
+                    Console.WriteLine($"Ответ от сервера {socket.LocalEndPoint} данные от клиента получены в {DateTime.Now}");
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            IPAddress address = Dns.GetHostAddresses(Dns.GetHostName())[2];                             
+            IPEndPoint endPoint = new IPEndPoint(address, 1024);
+            Socket pass_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            pass_socket.Bind(endPoint);
+            pass_socket.Listen(10);
+            Console.WriteLine($"Сервер начал прослушивание на порту 1024");
+            
+            try
+            {
+                int len;
+                while(true)
+                {
+                                       
+                    Socket socket = pass_socket.Accept();                  
+                    Console.WriteLine($"Подключился хост {socket.LocalEndPoint}");
+                    Console.WriteLine("Hello");
+                    Console.WriteLine($"Подключился хост {socket.LocalEndPoint}");                    
+                    byte[] buf = new byte[1024];
+                    int received = socket.Receive(buf);
+                    string message = Encoding.UTF8.GetString(buf, 0, received);
+                    Console.WriteLine($"{message}");
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+
+                Console.WriteLine(ex);
+            } 
+        }
+    }
+}
